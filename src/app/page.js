f4dba7c725767dda77b9377962b37d9cec4b4fd0
showrov4g <showrovg00@gmail.com>
2025-01-28 22:07:48 +0600

    table making

--- src/app/page.js
+++ src/app/page.js
@@ -25,16 +25,19 @@ export default function Home() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                ID:
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+               Title: 
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                description 
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Actions
               </th>
             </tr>
           </thead>
@@ -50,28 +53,7 @@ export default function Home() {
               <td className="px-6 py-4">Laptop</td>
               <td className="px-6 py-4">$2999</td>
             </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-            </tr>
+
           </tbody>
         </table>
       </div>
