97eeab0c446e59d3fa56407f17b6a366d5775dc4
showrov4g <showrovg00@gmail.com>
2025-02-01 23:15:32 +0600

    database connection successfully

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
+import { ConnectDB } from "../lib/config/db";
 
 const loadDb = async()=>{
-    
+    await ConnectDB()
 }
-
+loadDb()
 
 export async function GET(request){
 
